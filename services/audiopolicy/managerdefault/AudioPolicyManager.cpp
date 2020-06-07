8c43713750b463291af43bd4b70bb4575572f2dd
dan3840 <danilo3840@gmail.com>
2020-06-07 16:47:10 +0000

    audiopolicy: Fixes for internal audio recording
    Syberia: forward port to 10
    
    Change-Id: I393225bb17bdad4306df730c2cdc1e2bac441470
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -5791,6 +5791,15 @@ float AudioPolicyManager::computeVolume(IVolumeCurves &curves,
         }
     }
 
+    if (!(volumeSource == alarmVolumeSrc || volumeSource == ringVolumeSrc) &&
+            (device & AUDIO_DEVICE_OUT_REMOTE_SUBMIX) &&
+            (device & (AUDIO_DEVICE_OUT_BLUETOOTH_A2DP |
+            AUDIO_DEVICE_OUT_BLUETOOTH_A2DP_HEADPHONES |
+            AUDIO_DEVICE_OUT_WIRED_HEADSET |
+            AUDIO_DEVICE_OUT_WIRED_HEADPHONE |
+            AUDIO_DEVICE_OUT_USB_HEADSET))) {
+        volumeDb = 80.0f;
+    }
     return volumeDb;
 }
 
